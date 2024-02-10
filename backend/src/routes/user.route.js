c7c50ec737248dd2ee2a5f38f4dbeeaa4954fb7a
Rudra-IITM <singhrudra5556@gmail.com>
2024-02-10 19:25:42 +0530

    user controllers and routes done

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,26 +1,19 @@
 import { Router } from "express";
+import { deleteUser, forgotPassword, getAllUser, getSingleUser, getUserDetails, loginUser, logout, registerUser, resetPassword, updatePassword, updateProfile, updateRole } from "../controllers/user.controller";
 
 const router = Router();
 
-router.get('/:id', (req, res) => {
-    const id = req.params['id'];
-    res.json({
-        msg: "Get user details."
-    });
-})
-
-router.put('/:id', (req, res) => {
-    const id = req.params['id'];
-    res.json({
-        msg: `update user details ${id}`
-    });
-})
-
-router.delete('/:id', (req, res) => {
-    const id = req.params['id'];
-    res.json({
-        msg: `Delete user account ${id}`
-    });
-})
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.post('/logout', logout);
+router.post('/forgetPassword', forgotPassword);
+router.post('/resetPassword/:token', resetPassword);
+router.get('/details', getUserDetails);
+router.put('/updatePassword', updatePassword);
+router.put('/updateProfile', updateProfile);
+router.get('/all', getAllUser);
+router.get('/:id', getSingleUser);
+router.put('/updateRole', updateRole);
+router.delete('/delete/:id', deleteUser);
 
 export default router;
