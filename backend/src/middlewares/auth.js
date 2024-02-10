2d37bf57682ec9dcdbe01f75a9ec9530e1aa5c33
Rudra-IITM <singhrudra5556@gmail.com>
2024-02-10 20:15:45 +0530

    auth header change

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -4,7 +4,7 @@ import { ApiError } from '../utils/ApiError';
 import jwt from 'jsonwebtoken'
 
 const isAuthenticatedUser = asyncHandler( async(req, res, next) => {
-    const { token } = req.cookies;
+    const { token } = req.headers.token;
 
     if(!token){
         return next(new ApiError(401))
