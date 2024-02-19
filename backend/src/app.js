e35051a072a2bf06d15eba04a8e3903ce769c7c0
Rudra Pratap Singh <rudra@Rudras-MacBook-Air.local>
2024-02-20 03:45:20 +0530

    containerize frontend and backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true, 
+    credentials: true,
 }));
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true, limit: '16kb'}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static("public"));
 
 //routes
